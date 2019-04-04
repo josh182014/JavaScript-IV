c29c1a049c444c0331f804b6e06580678908d5f3
josh182014 <josh182014@gmail.com>
2019-04-04 14:37:49 -0600

    set up all of the classes and some console logs to test

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,103 @@
 // CODE here for your Lambda Classes
+//Classes
+//Main Person
+class Person{ 
+    constructor (mainAttributes) {
+        this.name = mainAttributes.name;
+        this.age = mainAttributes.age;
+        this.location = mainAttributes.location;
+        this.gender = mainAttributes.gender;
+    }
+    speak() {
+        return `Hello my name is ${this.name}, I am from ${this.location}.`;
+    }
+};
+
+//Instructor
+class Instructor extends Person{ 
+    constructor (mainAttributes) {
+        super(mainAttributes);
+        this.specialty = mainAttributes.specialty;
+        this.favLanguage = mainAttributes.favLanguage;
+        this.catchPhrase = mainAttributes.catchPhrase;
+    }
+    demo(subject) {
+        return `Today we are learning about ${subject}`;
+    }
+    grade(student,subject) {
+        return `${student.name} receives a perfect score on ${subject}.`;
+    }
+};
+
+//Student
+class Student extends Person { 
+    constructor (mainAttributes) {
+        super(mainAttributes);
+        this.previousBackground = mainAttributes.previousBackground;
+        this.className = mainAttributes.className;
+        this.favSubjects = mainAttributes.favSubjects;
+    }
+    listsSubjects() {
+        return this.favSubjects;
+    }
+    PRAssignment(subject) {
+        return `${this.name} has submitted a PR for ${subject}.`
+    }
+    sprintChallenge(subject) {
+        return `${this.name} has begun sprint challenge on ${subject}.`
+    }
+};
+
+//PM
+class PM extends Instructor { 
+    constructor (instructorAttributes) {
+        super(instructorAttributes);
+        this.gradClassName = instructorAttributes.gradClassName;
+        this.favInstructor = instructorAttributes.favInstructor;
+    }
+    standUp(channel) {
+        return `${this.name} announces to ${channel}, @channel standy times!​​​​​`;
+    }
+    debugsCode(student,subject) {
+        return `${this.name} debugs ${student.name}'s code on ${subject}`;
+    }
+};
+
+
+//Objects
+//Person
+const george = new Person({
+    name: 'George',
+    age: '98',
+    location: 'Hawaii',
+    gender: 'M'
+});
+//Instructor
+const daryl = new Instructor({
+    name: 'Daryl',
+    location: 'Hawaii'
+});
+//Student
+const dave = new Student({
+    name: 'Dave',
+    location: 'CA',
+    favSubjects: ['python','ruby']
+});
+
+//PM
+
+const smith = new PM({
+    name: 'Smith',
+    location: 'NY',
+    favInstructor: 'Instructor Josh',
+});
+
+
+//Logs
+console.log(george.speak());
+console.log(daryl.demo('Python'));
+console.log(daryl.grade(dave,'Python'));
+console.log(dave.listsSubjects());
+console.log(dave.PRAssignment('JavaScript'));
+console.log(dave.sprintChallenge('CSS'));
+console.log(smith.debugsCode(dave,'Ruby'));
